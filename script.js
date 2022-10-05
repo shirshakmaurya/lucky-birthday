10ff55088a3b14784721aa400cd2f07100ac5418
shirshak <84394640+shirshakmaurya@users.noreply.github.com>
2022-10-05 15:30:19 +0530

    Lucky birthday logic

---
+++ script.js
@@ -0,0 +1,32 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box")
+
+function compareValues(sum, luckyNumber) {
+  if (sum % luckyNumber == 0) {
+    outputBox.innerText = "Your birthday is lucky"
+  }
+  else {
+    outputBox.innerText = "Your birthday is not lucky"
+  }
+}
+
+function checkBirthdayDateIsLucky() {
+  const birthDate = dateOfBirth.value;
+  const sum = calculateSume(birthDate);
+  if(sum && birthDate)
+    compareValues(sum, luckyNumber.value);
+  else  
+    outputBox.innerText("Please enter both details.")
+}
+
+function calculateSume(birthDate) {
+  birthDate = birthDate.replaceAll("-", "");
+  let sum = 0;
+  for (let index = 0; index < birthDate.length; index++) {
+    sum = sum + Number(birthDate.charAt(index))
+  }
+  return sum;
+}
+checkNumberButton.addEventListener("click", checkBirthdayDateIsLucky)
